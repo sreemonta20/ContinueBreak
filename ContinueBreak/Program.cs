629ec0ca2cb130c40fde58e9f0073e6bfa382195
sreemonta20 <sbhowmikcse08@gmail.com>
2020-12-30 22:23:13 +0100

    Add project files.

---
+++ ContinueBreak/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ContinueBreak
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            for ( int i = 0; i <=5; i++)
+            {
+                if (i == 4)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(" In Break Value of i is {0}", i);
+                }
+                
+                
+
+            }
+
+
+            for (int j = 0; j <= 5; j++)
+            {
+                if (j == 4)
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine(" In Continue Value of J is {0}", j);
+                }
+                
+                
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# ContinueBreak
+initial change
+
+# How do they work
+Break is being used to immediate termination out of the loop. But continue helps to terminate the current execution and continues to get the control for the next iteration
